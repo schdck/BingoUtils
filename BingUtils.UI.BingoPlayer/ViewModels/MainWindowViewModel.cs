12adc215628d116019dc64af73d4bbdf1b4ad4f4
mvschmaedeck1 <mauricio.schmaedeck@gmail.com>
2016-07-28 00:53:13 -0300

    Adicionados componentes MainWindow

---
+++ BingUtils.UI.BingoPlayer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BingoUtils.Domain.Entities;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingUtils.UI.BingoPlayer.ViewModels
+{
+    public class MainWindowViewModel : DefaultViewModel
+    {
+        public List<Question> QuestionsList { get; private set; }
+
+        public string CurrentQuestionTitle { get; private set; }
+        public string PreviousQuestioTitle { get; private set; }
+        public string QuestionProgress { get; private set; }
+
+        public TransitionType AnimationToBeUsed { get; private set; }
+
+        public SimpleDelegateCommand RefreshQuestionCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            CurrentQuestionTitle = "Seja bem-vindo.\nPara iniciar um jogo clique no botão abaixo.";
+            QuestionProgress = "1/60";
+
+            RefreshQuestionCommand = new SimpleDelegateCommand((x) => CurrentQuestionTitle = "Olá");
+        }
+    }
+}
--- BingoUtils.Domain/Entities/DefaultViewModel.cs
+++ BingoUtils.Domain/Entities/DefaultViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace BingoUtils.Domain.Entities
 {
+    [ImplementPropertyChanged]
     public abstract class DefaultViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
