e58fafe238ed03f6312576dc4982acf324493f37
mvschmaedeck <mauricio.schmaedeck@gmail.com>
2017-03-08 10:14:27 -0300

    Started to add Multilanguage support

---
+++ BingoUtils.UI.BingoPlayer/Languages/LanguageMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BingoUtils.UI.BingoPlayer.Languages
+{
+    public static class LanguageMapper
+    {
+        private const string DictionariesLocation = "/Languages/Dictionaries/";
+
+        public static ResourceDictionary CurrentLanguage { get; private set; }
+
+        public static readonly ResourceDictionary PTBR = 
+            new ResourceDictionary() { Source = new Uri(Path.Combine(DictionariesLocation, "PT-BR"), UriKind.Relative) };
+        
+        public static readonly ResourceDictionary ENUS =
+            new ResourceDictionary() { Source = new Uri(Path.Combine(DictionariesLocation, "EN-US"), UriKind.Relative) };
+
+        static LanguageMapper()
+        {
+
+        }
+    }
+}
