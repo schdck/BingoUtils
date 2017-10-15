425034c718d5ceaf02d177d3b9e1b1ce61f65673
amandafbalbinot <amandabalbinot@hotmail.com>
2017-10-15 16:09:01 -0200

    Close #4

--- BingoUtils.UI.BingoPlayer/Resources/ResourceMapper.cs
+++ BingoUtils.UI.BingoPlayer/Resources/ResourceMapper.cs
@@ -11,6 +11,7 @@ namespace BingoUtils.UI.BingoPlayer.Resources
         public static readonly IEnumerable<string> ResourceFiles = new List<string>()
         {
             "História.Brasil Pós-Independência.zip",
+            "Química.Química Orgânica.zip"
         };
     }
 }
